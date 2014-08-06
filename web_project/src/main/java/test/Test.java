047852603d1e11292a7348fadc64cb790ca2a90f
Aliaksei_Vihuro <Aliaksei_Vihuro@EPBYMOGW0139.minsk.epam.com>
2014-08-06 11:26:39 +0300

    Add spring dependencies and logging

---
+++ web_project/src/main/java/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ * @author Aliaksei_Vihuro
+ */
+@Slf4j
+public class Test {
+    public static void main(String[] args) {
+        log.info("Hello!");
+    }
+}
