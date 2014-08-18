3198a58f1162fbfcbd692ecd0ef03cabbe7d85ec
Azargan <azargan@yandex.ru>
2014-08-18 14:54:37 +0300

    Configure Neo4j and Spring data.

---
+++ web_project/src/main/java/by/azargan/config/Neo4jConfig.java
@@ -0,0 +1,26 @@
+package by.azargan.config;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
+
+/**
+ *
+ * @author Aliaksei_Vihuro
+ */
+@Configuration
+@EnableNeo4jRepositories(basePackages = "by.azargan.repository")
+public class Neo4jConfig extends Neo4jConfiguration {
+
+    final private String DB_PATH = "D:\\test.db";
+
+    @Bean(destroyMethod = "shutdown")
+    GraphDatabaseService graphDatabaseService() {
+        setBasePackage("by.azargan.data");
+        return new GraphDatabaseFactory().newEmbeddedDatabase(DB_PATH);
+    }
+
+}
